815828a08732fb0c332297a6b693d948ce0baf4e
zhengluqian <2661837382@qq.com>
2024-06-25 16:40:33 +0800

    [Title]:完善服务提供者模块 [Description]:引入相关依赖，实现用户服务接口

---
+++ provider/src/main/java/com/nami/provider/ProviderExample.java
@@ -0,0 +1,13 @@
+package com.nami.provider;
+
+/**
+ * 简易服务提供者示例
+ *
+ * @author nami404
+ * * @date 2024/6/25 16:38
+ */
+public class ProviderExample {
+    public static void main(String[] args) {
+        // 之后在这里编写提供服务的代码
+    }
+}
